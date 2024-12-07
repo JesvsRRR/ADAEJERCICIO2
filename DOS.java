7e1fb307ae6b6be9d7d84c7d63477bace3838c82
JesvsRRR <jesusronaldrosales05@hotmail.com>
2024-12-06 21:42:59 -0500

    Add files via upload

---
+++ DOS.java
@@ -0,0 +1,41 @@
+package dos;
+
+import java.util.Scanner;
+
+public class DOS {
+
+    public static void Rotar(int d){
+    Scanner sc = new Scanner(System.in);    
+    int[][] Matriz = new int[d][d];
+    
+    
+    for(int i=0; i < d; i++){
+        for(int j=0; j < d;j++){
+            System.out.print("["+i+"]"+"[" + j+"]");
+            Matriz[j][d - i - 1] = sc.nextInt();           
+        }
+    }
+    System.out.println("MATRIZ ROTADA: ");
+    for(int i=0; i < d; i++){
+        for(int j=0; j < d;j++){
+            System.out.print("["+Matriz[i][j]+"]");        
+        }
+    System.out.println("");    
+    }
+    }
+
+    
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresar dimension [d]: ");
+        int d = sc.nextInt();
+        if(d <= 0){
+           d = sc.nextInt();;
+        }
+    
+    Rotar(d);
+        
+         
+    }
+    
+}
